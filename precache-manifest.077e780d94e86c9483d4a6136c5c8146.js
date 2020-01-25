696fa52d943ab1621d774c3725bf9bacde88d78d
darshankapadiya19 <darshankapadiya9@.com>
2020-01-25 16:33:33 +0530

    Updates

---
+++ precache-manifest.077e780d94e86c9483d4a6136c5c8146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e213e8f3b6fc89e2c619cb507b3a052",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7bb4bb8ba2afb5336c4a",
+    "url": "/monsters-rolodex/static/css/main.46d493f7.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bb4bb8ba2afb5336c4a",
+    "url": "/monsters-rolodex/static/js/main.1401cdca.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
