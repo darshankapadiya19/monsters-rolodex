68345aabb5117a8ff40165f03e24b2147f396ed9
darshankapadiya19 <darshankapadiya9@.com>
2020-01-25 12:20:08 +0530

    First add

---
+++ build/precache-manifest.a40e78206f7a16f506e6b9b01bfbc0f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fb63213da1f948e15baae5917580ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4470afa49b272e8e857",
+    "url": "/static/css/main.d1b83405.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4470afa49b272e8e857",
+    "url": "/static/js/main.a29c1c3c.chunk.js"
+  },
+  {
+    "revision": "249a37921e0703ab7925",
+    "url": "/static/js/runtime-main.04abcc89.js"
+  }
+]);
